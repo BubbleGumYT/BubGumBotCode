68bcfdc71c48e7d81a098374e5c2b9491e07e2a8
BubblGumYt <aaronieyt@gmail.com>
2021-04-01 10:24:46 -0700

    fixed some errors in welcome code

--- Commands/welcome.js
+++ Commands/welcome.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-const { Client, Message, TeamMember, GuildMember } = require("discord.js");
-
 module.exports = Client => {
     client.on('guildMemberAdd', guildMember =>{
         let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === 'Member');
@@ -8,8 +5,7 @@ module.exports = Client => {
         guildMember.roles.add(welcomeRole);
         guildMember.guild.channels.cache.get('Y816949158056820746').send(`Welcome <@${guildMember.user.id}> This is a server dedicated to Bub Gum Bot. The bot is still under development so we want your help. Your suggestions for bot commands, and codes, or even for the server all belong in the suggestion category. Thank you for joining us on this project!`)
     });
-=======
-const { Client, Message, TeamMember, GuildMember } = require("discord.js");
+const {Message, TeamMember, GuildMember } = require("discord.js");
 
 module.exports = Client => {
     client.on('guildMemberAdd', guildMember =>{
@@ -17,6 +13,4 @@ module.exports = Client => {
      
         guildMember.roles.add(welcomeRole);
         guildMember.guild.channels.cache.get('Y816949158056820746').send(`Welcome <@${guildMember.user.id}> This is a server dedicated to Bub Gum Bot. The bot is still under development so we want your help. Your suggestions for bot commands, and codes, or even for the server all belong in the suggestion category. Thank you for joining us on this project!`)
-    });
->>>>>>> fcedac138901597e7be12b9f7fee0613bea2d9bd
-}
+    })}};
