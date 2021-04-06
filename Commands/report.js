9ffae0be6f207ced1af7e247b87acd731e9d4437
BubblGumYt <aaronieyt@gmail.com>
2021-04-06 15:07:40 -0700

    it tells you the channel now!

--- Commands/report.js
+++ Commands/report.js
@@ -4,25 +4,24 @@ const { User, ClientUser, GuildMember, TeamMember, Message} = require("discord.j
 
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "USER", "REACTION"]});
 
-const guild = client.guilds.cache.get(process.env.DISCORD_SERVER_ID);
-
 module.exports = {
-
     name: 'report',
     description: "report a naughty person",
-    async execute(message, args, Discord, client){
-              
 
-        let embed = new Discord.MessageEmbed()
+    async execute(message, args, Discord, client){ 
+
+    const guild = message.guild   
+
+    let embed = new Discord.MessageEmbed()
         .setColor('#1ed700')
         .setTitle('Report \n')
         .setDescription(`Person who reported ${message.author} \n`
-            + `Channel reported in: channel\n`
-            + `Person reported: person \n`
-            + `Reason reported: Reason` )
+            + `Channel reported in: ${message.channel}\n`
+            + `Person reported: person \n` //The second argument
+            + `Reason reported: Reason` ) // The third argument
          
 
     let messageEmbed = await message.channel.send(embed);
-    message.channel.send('Hello!');
+    message.channel.send(`<@&${process.env.DUMMY_ROLE}>`);
 
-    }}
+    }};
